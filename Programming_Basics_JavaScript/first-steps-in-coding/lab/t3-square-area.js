9a40ba42d4e7341ee703bf9b27cc858f1cdda953
Iliana-Buchvarova <iliankabuchvarova952@abv.bg>
2023-05-08 23:11:47 +0300

    updated t3-square-area.js task in first-steps-in-coding lab

--- Programming_Basics_JavaScript/first-steps-in-coding/lab/t3-square-area.js
+++ Programming_Basics_JavaScript/first-steps-in-coding/lab/t3-square-area.js
@@ -1,10 +1,14 @@
-function area(vhodni_danni) {
-  let a = Number(vhodni_danni[0])
-  let result= a * a
-  console.log(result)
+function area(input) {
+  
+  let a = Number(input[0]);
+  let result = a * a;
+  
+  console.log(result);
+
 }
 
 
 
 
-area(["5", "55", "74", "100000000"])
+area(["5", "55", "74", "100000000"]);
+
